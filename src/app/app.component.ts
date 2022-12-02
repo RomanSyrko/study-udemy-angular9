1ce01dcca605d44dc5920375d6bc18b2cf38be89
RomanSyrko <syrko.roman@gmail.com>
2022-12-02 14:41:14 +0200

    Final this part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import {Component} from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MAIN';
+  title = 'Angular NgModels';
+
+  appState = 'on'
+
+  handChange() {
+    console.log(this.appState);
+  }
 }
