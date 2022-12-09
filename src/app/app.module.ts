8f80875eb89eaa6cb404b6016c9d4bdb8e61ff99
RomanSyrko <syrko.roman@gmail.com>
2022-12-09 15:28:32 +0200

    Main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,14 @@ import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms'
-import {HomePageComponent} from './home-page/home-page.component'
-import {AppRoutingModule} from './app-routing.module'
-import {AboutPageComponent} from './about-page/about-page.component'
-import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
-import {ColorDirective} from './shared/color.directive'
-import {PageNamePipe} from './shared/page-name.pipe'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent,
-    AboutPageComponent,
-    AboutExtraPageComponent,
-    ColorDirective,
-    PageNamePipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
