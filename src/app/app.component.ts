d8ee53f41c7e6da28ac5da4c4eadb7798872c072
RomanSyrko <syrko.roman@gmail.com>
2022-12-15 16:24:31 +0200

    Final this part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core'
 import {ModalComponent} from "./modal/modal.component";
 import {RefDirective} from "./ref.directive";
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,23 @@ export class AppComponent {
   /// Варіант №2
   @ViewChild(RefDirective) refDir: RefDirective
 
-  constructor(private resolver: ComponentFactoryResolver) {
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    /// Доступ до назви вкладки у браузері
+    private title: Title,
+    /// Мета теги
+    private meta: Meta
+  ) {
+    const t = title.getTitle();
+    console.log('title.getTitle():',t);
+    /// Змінюємо назву вкладки у браузері
+    this.title.setTitle('App Component Page!')
+
+    /// Додаємо ці мета теги в HTML -> <head></head>
+    this.meta.addTags([
+      {name: 'name', content: 'angular, google'},
+      {name: 'describtion', content: 'this is app component!'}
+    ])
   }
 
   showModal() {
